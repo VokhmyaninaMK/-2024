7f40e960c19bdf6899205e006778412defeafc8c
Masha <mashavokh@gmail.com>
2024-06-29 11:15:32 +0300

    сайт

---
+++ Сайт/correct_email.js
@@ -0,0 +1,38 @@
+function showDialog() {
+    if(postEmail())
+        {alert("Спасибо за комментарий!");
+    window.location.reload();
+}
+else
+    {alert("Некорректные данные");
+}
+}
+
+
+function postEmail() {
+    var str = document.getElementById("email").value;
+    var nm = document.getElementById("name").value;
+    var status = document.getElementById("status");
+    var statusName = document.getElementById("statusName");
+    var re = /^[^\s()<>@,;:\/]+@\w[\w\.-]+\.[a-z]{2,}$/i;
+    var res = true;
+    if (re.test(str)) {status.innerHTML = "Адрес правильный";
+        res = true;
+    }
+    else {status.innerHTML = "Адрес неверный";
+    if(postEmpty(str)) status.innerHTML = "Поле пустое";
+   
+        res = false;
+    }
+    statusName.innerHTML = "Имя правильное"
+    if(postEmpty(nm))
+        {
+            statusName.innerHTML = "Пустое имя";
+            res = false;
+        }
+        return res;
+}
+
+    function postEmpty(str){
+    return (str == null) || (str.length == 0);
+    }
